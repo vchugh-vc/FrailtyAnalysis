596a6816f55b89f4b229b18f24438e711d9ee163
vchugh-vc <chughspam@gmail.com>
2023-11-16 22:58:56 +0000

    DTW for comparison of an example file against new data (returns distance and path)

--- DTWMovement.py
+++ DTWMovement.py
@@ -1,27 +1,21 @@
+from dtaidistance import dtw
+from dtaidistance import dtw_visualisation as dtwvis
+import numpy as np
 import pandas as pd
-import numpy
-import matplotlib.pyplot as plt
-from scipy.spatial.distance import euclidean
-from fastdtw import fastdtw
 
-reference = "PitchTiltDTW.csv"
-plt.rcParams["figure.autolayout"] = True
-df = pd.read_csv(reference)
-Pitch = df[df.columns[0]]
-Roll = df[df.columns[1]]
-AccY = df[df.columns[2]]
-AccX = df[df.columns[3]]
-AccZ = df[df.columns[4]]
-GyroX = df[df.columns[5]]
-GyroY = df[df.columns[6]]
-GyroZ = df[df.columns[7]]
-print("Contents in csv file:")
-sampling_freq = 130
-x_time = numpy.arange(0, len(Roll) / sampling_freq, 1 / sampling_freq)
+IMUFile = "IMUData.csv"
+df = pd.read_csv(IMUFile)
+Pitch = df['Pitch']
 
-plt.subplot(2, 1, 1)
-plt.plot(x_time, Pitch, label="Pitch")
-plt.xlabel("Time (s)")
-plt.ylabel("Angle (°)")
-plt.legend(loc='center right')
-plt.show()
+ExampleFile = "PitchTiltDTW.csv"
+exampleDF = pd.read_csv(ExampleFile)
+ExamplePitch = exampleDF[df.columns[0]]
+
+
+s1 = ExamplePitch
+s2 = Pitch
+path = dtw.warping_path(s1, s2)
+distance = dtw.distance(s1,s2)
+dtwvis.plot_warping(s1, s2, path, filename="warp.png")
+
+print(distance)
