a89513bc5734e459bd5baf7ce7582c1a579b3422
vchugh-vc <chughspam@gmail.com>
2023-11-17 23:15:48 +0000

    MinMax Data Extraction

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -18,7 +18,7 @@ sampling_freq = 130
 x_time = numpy.arange(0, len(IMUAccY) / sampling_freq, 1 / sampling_freq)
 
 
-def filteration():
+def filtration():
     sos = signal.butter(1, 0.25, 'hp', fs=sampling_freq, output='sos')
     butter_x = signal.sosfilt(sos, IMUAccX)
     plt.subplot(4, 1, 1)
@@ -44,13 +44,13 @@ def filteration():
     return [butter_x, butter_y, butter_z]
 
 
-filtered_signals = filteration()
+filtered_signals = filtration()
 AccX = filtered_signals[0]
 AccY = filtered_signals[1]
 AccZ = filtered_signals[2]
 
 
-def SignalMagnitudeVector():
+def signal_magnitude_vector():
     signal_sum = pow(AccX, 2) + pow(AccY, 2) + pow(AccZ, 2)
     smv = numpy.sqrt(signal_sum)
     plt.subplot(4, 1, 4)
@@ -61,5 +61,24 @@ def SignalMagnitudeVector():
     return smv
 
 
-smv_data = SignalMagnitudeVector()
-print(smv_data)
+def minmax():
+    max_x = max(AccX)
+    min_x = min(AccX)
+    minmax_x = max_x-min_x
+    peak_x = max(max_x, -min_x)
+    print(f"Acc. X | Min = {min_x} | Max = {max_x} | MinMax = {minmax_x} | Peak = {peak_x}")
+    max_y = max(AccY)
+    min_y = min(AccY)
+    minmax_y = max_y - min_y
+    peak_y = max(max_y, -min_y)
+    print(f"Acc. Y | Min = {min_y} | Max = {max_y} | MinMax = {minmax_y} | Peak = {peak_y}")
+    max_z = max(AccZ)
+    min_z = min(AccZ)
+    minmax_z = max_z - min_z
+    peak_z = max(max_z, -min_z)
+    print(f"Acc. Z | Min = {min_z} | Max = {max_z} | MinMax = {minmax_z} | Peak = {peak_z}")
+
+
+minmax()
+smv_data = signal_magnitude_vector()
+
