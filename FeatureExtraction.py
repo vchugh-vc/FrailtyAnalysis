28429a41c0b2a426b965464923cfd83690efef2d
vchugh-vc <chughspam@gmail.com>
2023-11-19 23:10:15 +0000

    Restructed to use Classes from FeaturesClass. Graphs and SMV in file

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -21,34 +21,40 @@ AccZ = Features(IMUAccZ, status="raw", name='AccZ')
 
 x_time_filtered = numpy.arange(0, AccX.length / sampling_freq, 1 / sampling_freq)
 
+x_time_window = numpy.arange(0, AccX.length / sampling_freq, 0.4)
+
 
 def signal_magnitude_vector():
     signal_sum = pow(AccX.AccelData, 2) + pow(AccY.AccelData, 2) + pow(AccZ.AccelData, 2)
     smv_data = numpy.sqrt(signal_sum)
-    plt.subplot(2, 1, 2)
-    plt.plot(x_time_filtered, smv_data, label='filtered')
+    plt.subplot(3, 1, 2)
+    plt.plot(x_time_filtered, smv_data, label='SMV')
     plt.legend()
     plt.xlabel("Time (s)")
     plt.title("Signal Magnitude Vector")
     plt.ylabel("Acceleration (g)")
 
-
-
-
-def sliding_windows(data):
-    window_data = []
-    i = 0
-    while i < len(data):
-        window_small = data[i:i + 100]
-        window_data.append(window_small)
-        i += 50
-
-    return window_data
+    return smv_data
 
 SMV = signal_magnitude_vector()
 
+
+def window_graph_rms(window_data):
+    graph_array = []
+    for i in window_data:
+        graph_variable = Features(i, 'processed', '0')
+        graph_array.append(graph_variable.rms_data)
+    return graph_array
+
+def window_graph_var(window_data):
+    graph_array = []
+    for i in window_data:
+        graph_variable = Features(i, 'processed', '0')
+        graph_array.append(graph_variable.var_data)
+    return graph_array
+
 def grapher():
-    plt.subplot(2, 1, 1)
+    plt.subplot(3, 1, 1)
     plt.plot(x_time_filtered, AccX.AccelData, label="AccX")
     plt.plot(x_time_filtered, AccY.AccelData, label="AccY")
     plt.plot(x_time_filtered, AccZ.AccelData, label="AccZ")
@@ -56,7 +62,50 @@ def grapher():
     plt.xlabel("Time (s)")
     plt.title("Acceleration on Different Axis")
     plt.ylabel("Acceleration (g)")
+
+    plt.subplot(3, 1, 3)
+    plt.plot(x_time_filtered, AccX.derivative, label="JerkX")
+    plt.plot(x_time_filtered, AccY.derivative, label="JerkY")
+    plt.plot(x_time_filtered, AccZ.derivative, label="JerkZ")
+    plt.legend()
+    plt.xlabel("Time (s)")
+    plt.title("Jerk on Different Axis")
+    plt.ylabel("Jerk (g/s)")
+
     plt.show()
 
+
+
+
+def grapher_spread():
+    x_window_rms = window_graph_rms(AccX.window_data)
+    y_window_rms = window_graph_rms(AccY.window_data)
+    z_window_rms = window_graph_rms(AccZ.window_data)
+    plt.subplot(4, 1, 1)
+    plt.plot(x_time_window, x_window_rms,label="X RMS")
+    plt.plot(x_time_window, y_window_rms, label="Y RMS")
+    plt.plot(x_time_window, z_window_rms, label="Z RMS")
+    plt.legend()
+    plt.xlabel("Time (s)")
+    plt.title("RMS of Acceleration on Diff. Axis")
+    plt.ylabel("Acceleration (g)")
+
+    x_window_var = window_graph_var(AccX.window_data)
+    y_window_var = window_graph_var(AccY.window_data)
+    z_window_var = window_graph_var(AccZ.window_data)
+    plt.subplot(4, 1, 2)
+    plt.plot(x_time_window, x_window_var,label="X VAR")
+    plt.plot(x_time_window, y_window_var, label="Y VAR")
+    plt.plot(x_time_window, z_window_var, label="Z VAR")
+    plt.legend()
+    plt.xlabel("Time (s)")
+    plt.title("VAR of Acceleration on Diff. Axis")
+    plt.ylabel("Acceleration (g)")
+    plt.show()
+    plt.show()
+
+
 grapher()
+
+
 
