23d362d8094b21a6f4ef29d29b4aade4db78e15c
vchugh-vc <chughspam@gmail.com>
2023-11-26 23:12:37 +0000

    Writes Feature Data to a new CSV File

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -1,10 +1,19 @@
 from FeatureClass import DataPreparation, Features
+import pandas as pd
 
 sampling_freq = 130
 
+headers = ['AccX','AccY', 'AccZ', 'SMV','Jerk' ,'XMax', 'XMin', 'XMinMax','XPeak', 'XSTD','XRMS','XVAR','YMax', 'YMin','YMinMax','YPeak', 'YSTD','YRMS','YVAR','ZMax', 'ZMin', 'ZMinMax','ZPeak', 'ZSTD','ZRMS','ZVAR','smvMax', 'smvMin', 'smvMinMax','smvPeak', 'smvSTD','smvRMS','smvVAR']
+
+outputFile = "FeaturesData.csv"
 
 FilteredData = DataPreparation()
 DataFeatures = Features(FilteredData)
+data = DataFeatures.output
+print(data)
+df = pd.DataFrame(data)
+new = df.T
+new.to_csv(outputFile, header=headers)
 
 
 
