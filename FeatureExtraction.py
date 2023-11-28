b0a92f4d4e72778d14d09f9145e0e2c19a0c49c4
vchugh-vc <chughspam@gmail.com>
2023-11-28 00:26:32 +0000

    Changed file to new CSV export (based on changes in Class File)

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -1,23 +1,25 @@
 from FeatureClass import DataPreparation, Features
 import pandas as pd
+import csv
 
 sampling_freq = 130
 
-headers = ['AccX','AccY', 'AccZ', 'SMV','Jerk' ,'XMax', 'XMin', 'XMinMax','XPeak', 'XSTD','XRMS','XVAR','YMax', 'YMin','YMinMax','YPeak', 'YSTD','YRMS','YVAR','ZMax', 'ZMin', 'ZMinMax','ZPeak', 'ZSTD','ZRMS','ZVAR','smvMax', 'smvMin', 'smvMinMax','smvPeak', 'smvSTD','smvRMS','smvVAR']
+HEADERS = ['Xmax', 'Xmin', 'Xminmax','XPeak', 'Xrms','Xstd','Xvar', 'YMax', 'Ymin', 'Yminmax','YPeak', 'Yrms','Ystd','Yvar','Zmax', 'Zmin', 'Zminmax','ZPeak', 'Zrms','Zstd','Zvar','SMVzax', 'SMVmin', 'SMVminmax','SMVPeak', 'SMVrms','SMVstd','SMVvar', 'ManualLabel']
 
 outputFile = "FeaturesData.csv"
 
 FilteredData = DataPreparation()
 DataFeatures = Features(FilteredData)
-data = DataFeatures.output
-print(data)
-df = pd.DataFrame(data)
-new = df.T
-new.to_csv(outputFile, header=headers)
+data = DataFeatures.output2
+df = pd.DataFrame.from_dict([data])
 
 
 
+with open(outputFile, 'a') as f:  # You will need 'wb' mode in Python 2.x
+    w = csv.DictWriter(f, data.keys())
+    w.writerow(data)
 
+print(df)
 
 
 
