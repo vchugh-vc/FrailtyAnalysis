f2a56c2f149ae839b5d6ddd7fcecaeb7e087ec6b
vchugh-vc <chughspam@gmail.com>
2024-01-02 23:40:17 +0000

    Comments and Cleaning

--- SignalAnalysis.py
+++ SignalAnalysis.py
@@ -12,10 +12,12 @@ SAMPLE_FREQ = FilteredData.Sample_Freq
 def butter_lowpass(cutoff, fs, order=5):
     return butter(order, cutoff, fs=fs, btype='low', analog=False)
 
+
 def butter_lowpass_filter(data, cutoff, fs, order=5):
     b, a = butter_lowpass(cutoff, fs, order=order)
     y = lfilter(b, a, data)
     return y
+
 
 Jerk = DataFeatures.Jerk
 AccX = DataFeatures.AccX
@@ -26,17 +28,15 @@ print(f"Average Jerk of Movement: {Jerk_RMS} m/s-3")
 print(f"Length of movemenet {len(Jerk)} at {DataFeatures.time}")
 print(f"Dominant Freq is {DataFeatures.FFTFreq}")
 
-new = y = butter_lowpass_filter(AccX, DataFeatures.FFTFreq * 10, SAMPLE_FREQ, 6)
-plt.subplot(2,1,1)
-plt.plot(AccX)
-plt.ylim(-1,1)
-plt.subplot(2,1,2)
+# Filters Noise from Signal, so that a cleaner signal is produced
+# Can be used for sending movement to EdgeImpulse
+
+new = butter_lowpass_filter(AccZ, DataFeatures.FFTFreq * 10, SAMPLE_FREQ, 6)
+plt.subplot(2, 1, 1)
+plt.plot(AccZ)
+plt.subplot(2, 1, 2)
 plt.plot(new)
-plt.ylim(-1,1)
 plt.show()
-
-
-
 
 
 def minmax_spread(array):  # Returns Min-Max Data of an Array
@@ -47,8 +47,5 @@ def minmax_spread(array):  # Returns Min-Max Data of an Array
     rms_data = numpy.sqrt(numpy.mean(array ** 2))
     std_data = numpy.nanstd(array)
     var_data = numpy.nanvar(array)
-    print(f"Max: {max_data}, Min: {min_data}, Range : {minmax_data}, Peak {peak_data}, RMS {rms_data}, STD {std_data}, Var {var_data}")
-
-minmax_spread(new)
-minmax_spread(AccZ)
-
+    print(
+        f"Max: {max_data}, Min: {min_data}, Range : {minmax_data}, Peak {peak_data}, RMS {rms_data}, STD {std_data}, Var {var_data}")
