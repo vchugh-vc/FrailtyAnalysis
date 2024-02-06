2cb181e34f6a5a2906ccb1d543b2a4c1e6a8e47c
vchugh-vc <chughspam@gmail.com>
2024-02-06 22:56:16 +0000

    New File for MQTT Subscribing from the Arduino IMU

---
+++ MQTT.py
@@ -0,0 +1,36 @@
+# https://www.emqx.com/en/blog/how-to-use-mqtt-in-python
+
+from paho.mqtt import client as mqtt_client
+
+broker = 'test.mosquitto.org'
+port = 1883
+topic = "VarunChugh1"
+client_id = "VarunChugh"
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+run()
