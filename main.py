504b8bad8f4bc04436f74857d3f3b6aff6fe4e6e
vchugh-vc <chughspam@gmail.com>
2024-02-13 22:56:01 +0000

    Phases Timestamps in Main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from MQTT import *
 from FeatureClass import *
 from DTW import *
-import time
 
 
 
@@ -10,5 +9,7 @@ if __name__ == "__main__":
     mqtt_transmission()
     FilteredData = DataPreparation()
     AccZ = FilteredData.AccZ_Trimmed
-    DataTimeWarping(AccZ)
-    print("DTW")
+    DTWPhases = DataTimeWarping(AccZ)
+    up = [DTWPhases.movement_stamps[0],DTWPhases.movement_stamps[1]]
+    middle = [DTWPhases.movement_stamps[1], DTWPhases.movement_stamps[2]]
+    down = [DTWPhases.movement_stamps[2], DTWPhases.movement_stamps[3]]
