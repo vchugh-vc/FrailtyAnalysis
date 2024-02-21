5c26b08f1bc89bac40ed671e6feaccfbcf4f935c
vchugh-vc <chughspam@gmail.com>
2024-02-21 21:04:46 +0000

    Analyses Up and Middle Phases

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from MQTT import *
 from FeatureClass import *
 from DTW import *
-
-
-
+from FrailtyIndex import *
 
 if __name__ == "__main__":
     mqtt_transmission()
     FilteredData = DataPreparation()
     AccZ = FilteredData.AccZ_Trimmed
     DTWPhases = DataTimeWarping(AccZ)
-    up = [DTWPhases.movement_stamps[0],DTWPhases.movement_stamps[1]]
+    up = [DTWPhases.movement_stamps[0], DTWPhases.movement_stamps[1]]
     middle = [DTWPhases.movement_stamps[1], DTWPhases.movement_stamps[2]]
     down = [DTWPhases.movement_stamps[2], DTWPhases.movement_stamps[3]]
+    middle_data = Features(FilteredData, middle)
+    up_data = Features(FilteredData, up)
+    Frailty(up_data, 'up')
+    Frailty(middle_data, 'middle')
