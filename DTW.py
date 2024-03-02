b15d4770d5657ed6b5996b5c62e3e7d85a7555b1
vchugh-vc <chughspam@gmail.com>
2024-03-02 23:04:59 +0000

    Combining Peak Algorithm with DTW

--- DTW.py
+++ DTW.py
@@ -24,20 +24,25 @@ class DataTimeWarping:
 
     def __init__(self, AccZ, AccX):
 
+        self.lifting_up_peak = None
+        self.pour_start = None
+        self.lifting_down_peak = None
         self.movement_stamps = None
-        self.up_end = None
+        self.DTW_up_end = None
         self.down_start = None
         self.AccZ = AccZ
         self.AccX = AccX
         self.trimmed_axis = numpy.arange(0, len(self.AccZ) / SAMPLE_FREQ,
                                          1 / SAMPLE_FREQ)
-        self.PhaseUp()
+
         # self.DTWDown()
         # self.DTWDown2()
-        self.Down3()
-        self.up3()
+        self.PhaseUp()
+        self.Down_Peaks()
         self.movement_stamps
+        self.walking_filter()
         self.movement_phases()
+
 
     def DTWGeneral(self):  # DTW Comparing an entire signal against pre-recorded signals and return the best fit
         minimum = 100
@@ -74,14 +79,13 @@ class DataTimeWarping:
 
         print(f"Up Fast: From {data_range}, the distance is {distance}")
 
-        self.up3()
+        self.Up_Peaks()
         plt.suptitle('Lifting Graph (Fast)')
         print(data_range)
         plt.plot(DTWAccZ, label='DTW')
         plt.plot(self.AccZ[0:data_range], label='IMU')
         plt.legend()
         plt.show()
-
 
         return [data_range, minimum]
 
@@ -114,19 +118,18 @@ class DataTimeWarping:
 
         return [data_range, minimum]
 
+    def Up_Peaks(self, start_range=200):
 
-    def up3(self):
-
-        lifting_up_peak = signal.find_peaks(self.AccZ[0:200], prominence=0.05, height=0.02)
-        lifting_down_peak = signal.find_peaks(-self.AccZ[0:200], prominence=0.05)
+        lifting_up_peak = signal.find_peaks(self.AccZ[0:start_range], prominence=0.05, height=0.02)
+        lifting_down_peak = signal.find_peaks(-self.AccZ[0:start_range], prominence=0.05)
         j = 0.04
         while len(lifting_up_peak[0]) < 1:
-            lifting_up_peak = signal.find_peaks(self.AccZ[0:200], prominence=j, height=0.02)
+            lifting_up_peak = signal.find_peaks(self.AccZ[0:start_range], prominence=j, height=0.02)
             j = j - 0.01
 
         k = 0.04
         while len(lifting_down_peak[0]) < 1:
-            lifting_down_peak = signal.find_peaks(-self.AccZ[0:200], prominence=k)
+            lifting_down_peak = signal.find_peaks(-self.AccZ[0:start_range], prominence=k)
             k = k - 0.01
 
         print(f"Scipy Peak of {lifting_up_peak}")
@@ -136,6 +139,7 @@ class DataTimeWarping:
             if lifting_up_peak[1]['prominences'][i] >= prom:
                 prom = lifting_up_peak[1]['prominences'][i]
                 location = lifting_up_peak[0][i]
+        self.lifting_up_peak = location
 
         print(f"UP3: Main Up Peak at {location} with Prom = {prom}")
 
@@ -146,22 +150,29 @@ class DataTimeWarping:
                 location = lifting_down_peak[0][i]
 
         print(f"UP3: Main Down Peak at {location} with Prom = {prom}")
-
-        plt.plot(self.AccZ[0:200])
-        plt.show()
-
+        self.lifting_down_peak = location
 
     def PhaseUp(self):
 
         fast = self.DTWUpFast()
         slow = self.DTWUpSlow()
+        self.Up_Peaks()
 
         if fast[1] <= slow[1]:
-            self.up_end = fast[0]
+            self.DTW_up_end = fast[0]
             print("Fast")
         else:
-            self.up_end = slow[0]
+            self.DTW_up_end = slow[0]
             print("Slow")
+        print(f"DTW UP End: {self.DTW_up_end}")
+
+        if self.lifting_down_peak < self.lifting_up_peak:
+            print(f"Rerunning Peaks Algorithm")
+            self.Up_Peaks(start_range=250)
+
+
+        if self.lifting_down_peak > self.DTW_up_end:
+            self.DTW_up_end = self.lifting_down_peak + 10
 
     def DTWDown(self):  # Compares a known down signal to different intervals of a movement
 
@@ -207,8 +218,7 @@ class DataTimeWarping:
         plt.legend()
         plt.show()
 
-
-    def Down3(self):
+    def Down_Peaks(self):
 
         up_peak = signal.find_peaks(self.AccZ[-200:], height=0.01, prominence=0.1)
         # prom = signal.peak_prominences(self.AccZ[-200:], up_peak[0])
@@ -223,8 +233,6 @@ class DataTimeWarping:
 
         print(f"UP3: Main Up Peak at {peak_location} with Prom = {prom}")
 
-
-
         location = len(self.AccZ) - (210 - peak_location)
         print(location)
         self.down_start = location
@@ -234,6 +242,19 @@ class DataTimeWarping:
         plt.legend()
         plt.show()
 
+    def walking_filter(self):
+
+        i = 20
+        while i <= 300:
+            Zgrad = numpy.gradient(self.AccZ[self.DTW_up_end + i:self.DTW_up_end + 20 + i])
+            Zgradient = numpy.mean(Zgrad) * 10000
+            Xgrad = numpy.gradient(self.AccX[self.DTW_up_end:self.DTW_up_end + i])
+            Xgradient = numpy.mean(Xgrad) * 10000
+            print(f"{self.DTW_up_end + i}:  X Grad {Xgradient} & Z Grad {Zgradient}")
+            i = i + 10
+
+        self.pour_start = self.DTW_up_end + 50
+
     def movement_phases(self):
 
         # plt.plot(AccX[self.up_end:self.down_start])
@@ -241,22 +262,13 @@ class DataTimeWarping:
         # plt.plot(AccZ[self.up_end:self.down_start])
         # plt.show()
 
-        self.movement_stamps = [0, self.up_end, self.down_start, len(self.AccZ)]
-        i = 20
-        while i <= 300:
-            Zgrad = numpy.gradient(self.AccZ[self.up_end + i :self.up_end + 20 + i])
-            Zgradient = numpy.mean(Zgrad) * 10000
-            Xgrad = numpy.gradient(self.AccX[self.up_end:self.up_end + i])
-            Xgradient = numpy.mean(Xgrad) * 10000
-            print(f"{self.up_end + i}:  X Grad {Xgradient} & Z Grad {Zgradient}")
-            i = i + 10
+        self.movement_stamps = [0, self.DTW_up_end, self.pour_start, self.down_start, len(self.AccZ)]
 
         for point in self.movement_stamps:
             plt.axvline(x=point, color='r', linestyle='--', label=f'x = {point}')
 
         plt.plot(self.AccZ)
         plt.show()
-
 
         # for i in range(len(self.movement_stamps) - 1):
         #     print(f"{self.movement_stamps[i]} and {self.movement_stamps[i + 1]}")
