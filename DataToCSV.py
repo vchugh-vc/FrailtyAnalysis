60d304c6e6e88d462fa003a963991e668a5d8509
vchugh-vc <chughspam@gmail.com>
2024-03-06 18:22:01 +0000

    New format for saving data

--- DataToCSV.py
+++ DataToCSV.py
@@ -1,4 +1,3 @@
-from FeatureClass import DataPreparation, Features
 import pandas as pd
 
 from datetime import date
@@ -9,6 +8,10 @@ print("Current date is:", current_date)
 current_csv = 'IMUData.csv'
 df = pd.read_csv(current_csv)
 
-new_csv = f'CollectionData/{current_date}-Subject-Trial-Repetition-Comment.csv'
+participant_ID = ""
+hand = ""
+trial = ""
+
+new_csv = f'CollectionData/{current_date}-{participant_ID}-{hand}-{trial}.csv'
 df.to_csv(new_csv, index=False)
-print('Saved Data to New File')
+print(f'Saved Data to New File to {new_csv}')
