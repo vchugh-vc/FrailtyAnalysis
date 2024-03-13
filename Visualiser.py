ae85a105150a27858d208472df21ed87f73729f5
vchugh-vc <chughspam@gmail.com>
2024-03-13 12:24:37 +0000

    Double Overlay of Radar Plots (left/right)

--- Visualiser.py
+++ Visualiser.py
@@ -1,13 +1,37 @@
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
-import plotly.graph_objs as go
+import plotly.graph_objects as go
 import plotly.express as px
 
-theta = ["UpAccZ","UpDelta","UpSPARC","UpRoll","UpPitch","MiddleDelta","MiddleAccZ","MiddleSPARC"]
-r = [0.813,0.574,0.641,0.422,0.879,0.884,0.661,0.278]
+theta = ["Date", "UpAccZ","UpDelta","UpSPARC","UpRoll","UpPitch","MiddleDelta","MiddleAccZ","MiddleSPARC"]
+r = [1,0.813,0.574,0.641,0.422,0.879,0.884,0.661,0.278]
+r2 = [2,0.413,0.274,0.141,0.622,0.279,0.184,0.161,0.178]
 
+df = pd.read_csv('FrailtyParameters.csv')
 
-df = pd.read_csv('FrailtyParameters.csv', parse_dates=True)
+fig = go.Figure()
+
+fig.add_trace(go.Scatterpolar(
+      r=r[1:],
+      theta=theta[1:],
+      fill='toself',
+      name='Left'
+))
+fig.add_trace(go.Scatterpolar(
+      r=r2[1:],
+      theta=theta[1:],
+      fill='toself',
+      name='Right'
+))
+
+fig.update_layout(
+  polar=dict(
+    radialaxis=dict(
+      visible=True,
+      range=[0, 1]
+    )),
+  showlegend=True
+)
 
 app = Dash(__name__)
 
@@ -15,7 +39,7 @@ app = Dash(__name__)
 app.layout = html.Div([
     html.Div(children='My First App with Data and a Graph'),
     dash_table.DataTable(data=df.to_dict('records'), page_size=10),
-    dcc.Graph(figure=px.line_polar(df, r=r, theta=theta, line_close=True))
+    dcc.Graph(figure=fig)
 ])
 
 # Run the app
