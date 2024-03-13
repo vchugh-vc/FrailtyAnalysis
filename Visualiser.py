5be6d8926a5ac8b9b337026b8aa9673b4594a047
vchugh-vc <chughspam@gmail.com>
2024-03-13 00:02:38 +0000

    Changes to Radar Plot

--- Visualiser.py
+++ Visualiser.py
@@ -1,29 +1,23 @@
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
+import plotly.graph_objs as go
 import plotly.express as px
 
+theta = ["UpAccZ","UpDelta","UpSPARC","UpRoll","UpPitch","MiddleDelta","MiddleAccZ","MiddleSPARC"]
+r = [0.813,0.574,0.641,0.422,0.879,0.884,0.661,0.278]
 
-df = pd.read_csv('FrailtyParameters.csv')
+
+df = pd.read_csv('FrailtyParameters.csv', parse_dates=True)
 
 app = Dash(__name__)
 
+# App layout
 app.layout = html.Div([
-    html.Div(children='Frailty Data'),
-    html.Hr(),
-    dcc.RadioItems(options=['value', 'score'], value='value', id='buttons'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
-    dcc.Graph(figure={}, id='graph')
+    html.Div(children='My First App with Data and a Graph'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+    dcc.Graph(figure=px.line_polar(df, r=r, theta=theta, line_close=True))
 ])
 
-
-@callback(
-    Output(component_id='graph', component_property='figure'),
-    Input(component_id='buttons', component_property='value')
-)
-def update_graph(col_chosen):
-    fig = px.line_polar(df, r = 'score', theta = 'Parameter', line_close = True)
-    return fig
-
-
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
