c297da46e1d10327dedfa6b3990301dd6fbd70e6
vchugh-vc <chughspam@gmail.com>
2024-03-30 20:55:17 +0000

    File for analysing all long term data

--- LongAnalysis.py
+++ LongAnalysis.py
@@ -13,15 +13,18 @@ FOLDER = 'LongitudinalData'
 files = os.listdir(FOLDER)
 files_order = sorted(files)
 
-for i in range(140,len(files_order)):
+for i in range(146, len(files_order)):
     print(f"{i}: {files_order[i]}")
+
+    name_parts = os.path.splitext(files_order[i])[0]
+    date_and_time = name_parts.rsplit('-', 1)[0]
 
     FilteredData = DataPreparation(file=f"LongitudinalData/{files_order[i]}")
     AccZ = FilteredData.AccZ_Trimmed
     AccX = FilteredData.AccX_Trimmed
     DTWPhases = DataTimeWarping(AccZ, AccX)
     TimeStamps = DTWPhases.movement_stamps
-    # TimeStamps = [0, 97, 107, 147, 167, 902, 1100]
+    # TimeStamps = [0, 31, 68, 78, 138, 841, 1046]
     up_data = Features(FilteredData, TimeStamps, 'up')
     middle_data = Features(FilteredData, TimeStamps, 'middle')
-    Frailty(up_data, middle_data)
+    Frailty(up_data, middle_data, date=date_and_time)
